30d94214c833dc0a4850c3717d0b4ffcd8a39e5f
Derrick Hubbard <derrickhubb@gmail.com>
2018-09-27 10:29:01 -0400

    Add project files.

---
+++ Exercise 8C Roulettegame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8C_Roulettegame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
